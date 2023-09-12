f57b9716bd349b1a8f7428016e343c9e6e44c8bb
chorongfire33 <choyeon8317@naver.com>
2023-09-12 17:47:17 +0900

    Feat: 로그인페이지 기능구현(수정요망)

--- blog_project/blog_app/views.py
+++ blog_project/blog_app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, redirect
 from .forms import PostForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def login(request):
-    return render(request, "login.html")
+    if request.method == "POST":
+        form == AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return render(request, "login.html")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "login.html", {"form": form})
+
+
+# def custom_logout(request):
+#     logout(request)
+#     return redirect("board_client")
 
 
 def board_admin(request):
