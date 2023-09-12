8cbd81b6ed0f5dba14e3e65126ab5c6485a550c9
kimziho <rlawlgh1027@naver.com>
2023-09-12 17:07:22 +0900

    Design: login css, js 추가 & 수정

---
+++ blog_project/blog_app/static/js/login.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName('input')[1].placeholder = 'username';
+document.getElementsByTagName('p')[0].style.marginTop = '3rem';
+document.getElementsByTagName('input')[2].placeholder = 'password';
