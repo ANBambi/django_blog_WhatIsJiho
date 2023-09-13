9fedc4c5702f6d0d0991a6a2b852a3135e6273d7
chorongfire33 <choyeon8317@naver.com>
2023-09-13 15:05:53 +0900

    Feat: models.py 업로드 시간 에러 해결

--- blog_project/blog_app/models.py
+++ blog_project/blog_app/models.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    create_at = models.DateTimeField('date created', auto_now_add=True, default=timezone.now)
+    create_at = models.DateTimeField('date created', auto_now_add=True)
 
     def __str__(self):
         return self.title
